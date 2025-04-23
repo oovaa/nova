383ba62c8274e7277d59af92bad5f8b668a82149
Omar <omarvx211@gmail.com>
2025-04-23 11:56:15 +0200

    feat: Add Analytics component to App for enhanced tracking

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'next-themes'
 import Index from './pages/Index'
 import NotFound from './pages/NotFound'
+import { Analytics } from '@vercel/analytics/react'
 
 const queryClient = new QueryClient()
-
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -25,6 +25,7 @@ const App = () => (
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
+    <Analytics />
   </QueryClientProvider>
 )
 
